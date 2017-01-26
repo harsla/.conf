669f5933d1ed819685effeeb7d1b442793c583ee
Scott Harsla <harsla@gmail.com>
2017-01-26 15:27:03 -0700

    fix zsh osx install

--- platform/osx/bootstrap.sh
+++ platform/osx/bootstrap.sh
@@ -1,5 +1,4 @@
-s
-urce $DOTCONF/common/all.sh
+source $DOTCONF/common/all.sh
 
 brew install tmux vim zsh zsh-completions tree wget nmap the_silver_searcher ack tmux reattach-to-user-namespace tidy-html5 entr
 
