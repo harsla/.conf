27f435e6e421c98182011d8c4318d801f1628515
Scott Harsla <harsla@gmail.com>
2017-04-09 19:30:50 -0700

    zsh windows edits

--- platform/deb/bootstrap.sh
+++ platform/deb/bootstrap.sh
@@ -1,3 +1,7 @@
 source $DOTCONF/common/all.sh
 
-sudo apt-get install -y git git-core vim zsh && wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh && chsh -s `which zsh`
+sudo apt-get install -y git git-core vim zsh && \
+wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh && \
+echo $(which zsh) | sudo tee -a /etc/shells && \
+chsh -s $(which zsh) && \
+cd .oh-my-zsh/themes/ && wget --no-check-certificate  https://raw.githubusercontent.com/babun/babun/master/babun-core/plugins/oh-my-zsh/src/babun.zsh-theme
