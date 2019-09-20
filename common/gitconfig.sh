b05d882529d6ada57e18eb24e5539e94b283c2b0
Scott Harsla <3027413+harsla@users.noreply.github.com>
2019-09-20 10:47:29 -0700

    work edits

--- common/gitconfig.sh
+++ common/gitconfig.sh
@@ -1,7 +1,7 @@
 git config --global core.editor "vim"
 git config --global color.diff true
 git config --global diff.submodule log
-git config --global user.email "harsla@gmail.com"
+git config --global user.email "3027413+harsla@users.noreply.github.com"
 git config --global user.name "Scott Harsla"
 git config --global push.default matching
 git config --global core.excludesfile $DOTCONF/dotfiles/gitignore_global
