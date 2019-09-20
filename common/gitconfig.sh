534061c09514dc4ba15284afb95128adeb94b376
Scott Harsla <3027413+harsla@users.noreply.github.com>
2019-09-20 11:01:07 -0700

    lets sign commits by default

--- common/gitconfig.sh
+++ common/gitconfig.sh
@@ -1,5 +1,6 @@
 git config --global core.editor "vim"
 git config --global color.diff true
+git config --global commit.gpgsign true
 git config --global diff.submodule log
 git config --global user.email "3027413+harsla@users.noreply.github.com"
 git config --global user.name "Scott Harsla"
